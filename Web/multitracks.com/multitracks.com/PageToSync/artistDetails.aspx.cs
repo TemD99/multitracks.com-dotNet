d1f7ad5850695b3668d578c221bf4a6442875a4e
TemD99 <48262807+TemD99@users.noreply.github.com>
2023-07-25 14:05:53 -0400

    artistdetails page fixes

--- Web/multitracks.com/multitracks.com/PageToSync/artistDetails.aspx.cs
+++ Web/multitracks.com/multitracks.com/PageToSync/artistDetails.aspx.cs
@@ -25,7 +25,7 @@ using DataAccess;
                 // Only executes if it's not a post back
                 if (!IsPostBack)
                 {
-                    LoadDefault();
+                   
 
                     // Check if the artist ID in the query string is valid
                     if (HasValidArtistId(out int artistID))
@@ -43,60 +43,89 @@ using DataAccess;
             }
         }
 
-        // Check if the artist ID in the query string is valid
-        private bool HasValidArtistId(out int artistID)
+    // Check if the artist ID in the query string is valid
+    private bool HasValidArtistId(out int artistID)
+    {
+        // Try to get the artist ID from the query string
+        string artistIDString = Request.QueryString["artistID"];
+        if (!string.IsNullOrEmpty(artistIDString) && int.TryParse(artistIDString, out artistID))
         {
-            // Try to get the artist ID from the query string
-            string artistIDString = Request.QueryString["artistID"];
-            if (!string.IsNullOrEmpty(artistIDString) && int.TryParse(artistIDString, out artistID))
-            {
-                return true;
-            }
-
-            // Display an alert if the artist ID is invalid
-            Response.Write("<script>alert('Please Enter a valid artistID.');</script>");
-            artistID = 0;
-            return false;
+            return true;
         }
 
-    
+        // If no artistID is found in the query string, set it to 1 (default artist ID)
+        artistID = 1;
+        return true;
+    }
+
 
     // Load artist details using the provided ID
     private void LoadArtistDetails(int artistID)
     {
-        // Create a new SQL data access object
-        SQL dataAccess = new SQL();
-
-        // Create a parameter for the stored procedure
-        SqlParameter parameter = new SqlParameter("@ArtistId", artistID);
-        dataAccess.Parameters.Add(parameter);
-
-        // Execute the stored procedure and get the result
-        DataSet artistDetailsAndSongs = dataAccess.ExecuteStoredProcedureDS("GetArtistDetails");
-        dataAccess.Parameters.Clear();
-
-        // Check if any artist details were returned
-        if (artistDetailsAndSongs.Tables[0].Rows.Count > 0)
+        try
         {
-            // Get the artist information from the first row
-            GetArtistInfo(artistDetailsAndSongs.Tables[0].Rows[0]);
-        }
+            // Create a new SQL data access object
+            SQL dataAccess = new SQL();
 
-        // Check if any songs were returned
-        if (artistDetailsAndSongs.Tables.Count > 1)
-        {
-            // Get the songs and bind them to the repeater
-            GetSongs(artistDetailsAndSongs.Tables[1], artistDetailsAndSongs.Tables[2]);
-            BindSongs();
-        }
+            // Create a parameter for the stored procedure
+            SqlParameter parameter = new SqlParameter("@ArtistId", artistID);
+            dataAccess.Parameters.Add(parameter);
 
-        // Check if any albums were returned
-        if (artistDetailsAndSongs.Tables.Count > 2)
-        {
-            // Bind albums to the repeater
-            albumsRepeater.DataSource = artistDetailsAndSongs.Tables[2];
-            albumsRepeater.DataBind();
+            // Execute the stored procedure and get the result
+            DataSet artistDetailsAndSongs = dataAccess.ExecuteStoredProcedureDS("GetArtistDetails");
+            dataAccess.Parameters.Clear();
+
+            // Check if any artist details were returned
+            if (artistDetailsAndSongs.Tables[0].Rows.Count > 0)
+            {
+                // Get the artist information from the first row
+                GetArtistInfo(artistDetailsAndSongs.Tables[0].Rows[0]);
+
+                // Check if any songs were returned
+                if (artistDetailsAndSongs.Tables.Count > 1)
+                {
+                    // Get the songs and bind them to the repeater
+                    GetSongs(artistDetailsAndSongs.Tables[1], artistDetailsAndSongs.Tables[2]);
+                    BindSongs();
+                }
+
+                // Check if any albums were returned
+                if (artistDetailsAndSongs.Tables.Count > 2)
+                {
+                    // Bind albums to the repeater
+                    albumsRepeater.DataSource = artistDetailsAndSongs.Tables[2];
+                    albumsRepeater.DataBind();
+                }
+            }
+            else
+            {
+                // Handle the case when no artist details are found for the given ID
+                Response.Write("<script>alert('Artist details not found for the provided ID. Loading default artist ID.');</script>");
+               
+                    // Load artist details using the ID
+                    LoadArtistDetails(1);
+
+                
+            }
         }
+        catch (Exception ex)
+        {
+            // Handle any exceptions that occur
+            HandleError(ex);
+        }
+    }
+
+    // Get artist information from a DataRow
+    private void GetArtistInfo(DataRow row)
+    {
+        // Set the artist's banner image
+        SetArtistBanner(Convert.ToString(row["heroURL"]));
+        // Set the artist's image
+        SetArtistImage(Convert.ToString(row["imageURL"]));
+        // Set the artist's name
+        artistName.Text = Convert.ToString(row["title"]);
+        // Set the artist's biography
+        litBiography.Text = GetFormattedBiography(Convert.ToString(row["biography"]));
     }
 
     // Bind the songs to the repeater
@@ -132,17 +161,7 @@ using DataAccess;
         }
 
         // Get artist information from a DataRow
-        private void GetArtistInfo(DataRow row)
-        {
-            // Set the artist's banner image
-            SetArtistBanner(Convert.ToString(row["heroURL"]));
-            // Set the artist's image
-            SetArtistImage(Convert.ToString(row["imageURL"]));
-            // Set the artist's name
-            artistName.Text = Convert.ToString(row["title"]);
-            // Set the artist's biography
-            litBiography.Text = GetFormattedBiography(Convert.ToString(row["biography"]));
-        }
+        
 
         // Set the artist's banner image
         private void SetArtistBanner(string artistHeroImage)
@@ -163,29 +182,5 @@ using DataAccess;
         }
 
         // Load the default values
-        private void LoadDefault()
-        {
-            // Set the default banner image
-            SetArtistBanner("./img/31.jpg");
-            // Set the default artist image
-            SetArtistImage("./img/174.jpg");
-        // Set the default biography
-        string bio = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>" +
-
-"<p>\"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?\"</p>" +
-
-"<p></p>";
-        litBiography.Text = GetFormattedBiography(bio);
-
-            // Set the default songs
-            Songs = new List<Song>()
-        {
-            new Song { SongInfo = "<span class='song-title'>Default Song 1</span> - <span class='song-bpm'>120</span> - <span class='song-time-signature'>4/4</span>"},
-            new Song { SongInfo = "<span class='song-title'>Default Song 2</span> - <span class='song-bpm'>100</span> - <span class='song-time-signature'>3/4</span>"},
-            new Song { SongInfo = "<span class='song-title'>Default Song 3</span> - <span class='song-bpm'>140</span> - <span class='song-time-signature'>4/4</span>"}
-        };
-
-            // Bind the default songs to the repeater
-            BindSongs();
-        }
+        
     }
